8fa9ead14ab6e52774f2fb1829f5419658d053a2
Nathan Garvie <nathang87@gmail.com>
2015-12-05 18:08:31 -0800

    Badge notifcation count clears properly

--- Moodrn/AppDelegate.swift
+++ Moodrn/AppDelegate.swift
@@ -48,6 +48,11 @@ import ParseFacebookUtilsV4
         installation.saveInBackground()
     }
     
+    //func application(application: UIApplication, didReceiveRemoteNotification: NSDictionary) {
+    //    let push = PFPush()
+    //    push.handlePush([NSObject : AnyObject]?)
+    //}
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
@@ -65,6 +70,12 @@ import ParseFacebookUtilsV4
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         FBSDKAppEvents.activateApp()
+        let currentInstallation = PFInstallation.currentInstallation()
+        if currentInstallation.badge != 0 {
+            currentInstallation.badge = 0
+            currentInstallation.saveEventually()
+            print("Notification badge cleared")
+        }
     }
 
     func applicationWillTerminate(application: UIApplication) {
