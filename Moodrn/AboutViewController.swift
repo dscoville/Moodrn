ef616ae90f99379498a8a54964d412f4b1c49dba
valeriechao <valeriechao@valeriechao-mbp.local>
2015-12-07 16:34:45 -0800

    Adding Settings

---
+++ Moodrn/AboutViewController.swift
@@ -0,0 +1,40 @@
+//
+//  AboutViewController.swift
+//  Moodrn
+//
+//  Created by Valerie Chao on 12/7/15.
+//  Copyright © 2015 David Scoville. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func didPressBack(sender: AnyObject) {
+        navigationController!.popViewControllerAnimated(true)
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SettingsViewController.swift
+++ SettingsViewController.swift
@@ -13,9 +13,13 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
 
     
     @IBOutlet weak var tableView: UITableView!
+
     
-    var titles: [String]!
-    var subtitles: [String]!
+    var objects = [
+        ["title" : "Notifications", "subtitle" : "Choose when you get notifications"],
+        ["title" : "About", "subtitle" : "Learn more about Moodrn"],
+        ["title" : "Log Out", "subtitle" : "Logged in as Valerie"]]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,14 +29,17 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
 //        nameLabel.text = "\(userEmail)"
 //        print("\(userEmail)")
         
-        titles = ["Notifications", "Log Out"]
-        subtitles = ["Choose when you get notifications, Logged in as \(userEmail)"]
+
 
         tableView.delegate = self
         tableView.dataSource = self
         
+        
+        UIApplication.sharedApplication().statusBarStyle = .LightContent
+        
+        
 
-
+    
         
         
         
@@ -41,6 +48,13 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        UIApplication.sharedApplication().statusBarStyle = UIStatusBarStyle.Default
+        
     }
     
     
@@ -53,23 +67,62 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
     
     // how many rows?
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return titles.count
+        return objects.count
     }
     
     // create the cells
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        self.tableView.separatorColor = UIColor(red: 1, green:1, blue:1, alpha: 0.3)
+        
         var cell = tableView.dequeueReusableCellWithIdentifier("SettingsCell") as! SettingsCell
         
-        var title = titles[indexPath.row]
-        cell.titleLabel.text = title
+        
+        let object = objects[indexPath.row]
+        
+        cell.titleLabel?.text = object["title"]!
+        cell.subtitleLabel?.text = object["subtitle"]!
+        
+        
+        if cell.titleLabel.text == "Log Out" {
+            cell.accessoryType = UITableViewCellAccessoryType.None
+        }
+        
+        
+        let backgroundView = UIView()
+        backgroundView.backgroundColor = UIColor(red: 0.3608, green: 0.0314, blue: 0.9059, alpha: 1.0) /* #5c08e7 */
+        cell.selectedBackgroundView = backgroundView
+        
 
-//        
-//        var subtitle = subtitles[indexPath.row]
-//        cell.subtitleLabel.text = subtitle
+        
         
         return cell
     }
     
+    
+    
+
+
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        let row = indexPath.row
+
+        if row == 0 {
+            print("Notifications")
+            self.performSegueWithIdentifier("notificationsSegue", sender: self)
+        } else if row == 1 {
+            print("About")
+            self.performSegueWithIdentifier("aboutSegue", sender: self)
+        } else if row == 2 {
+            print("Log out")
+        } else {
+            print ("Not working")
+        }
+        
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        
+    }
     
 
 }
